b77fd447f34f0682887813bab20267d02d54b185
James Page <james.page@canonical.com>
2011-06-10 17:42:53 +0100

    Initial re-shuffle

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-jenkins',
+      version='0.1',
+      description='Python bindings for the remote Jenkins API',
+      author='Ken Conley',
+      author_email='kwc@willowgarage.com',
+      url='http://launchpad.net/python-jenkins',
+      packages=['jenkins'],
+     )
