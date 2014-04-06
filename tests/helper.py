0d391899183c39a3fbdfbc34e3e413d32bfdc754
Marc Abramowitz <marca@surveymonkey.com>
2014-04-06 08:34:22 -0700

    Add tests, make it work on Python 3
    Change-Id: Ia8eedf699e988a199520c48e21c9731af912003c

--- tests/helper.py
+++ tests/helper.py
@@ -3,3 +3,10 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 import jenkins  # noqa
+
+try:
+    # Python 2
+    from StringIO import StringIO  # noqa
+except ImportError:
+    # Python 3
+    from io import StringIO  # noqa
