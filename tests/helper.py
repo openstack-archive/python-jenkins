0173f1afc446548a58fb4a0e05474c72fd8281b4
Marc Abramowitz <marca@surveymonkey.com>
2014-04-06 08:34:22 -0700

    Make it work on Python 3
    Change-Id: Ia8eedf699e988a199520c48e21c9731af912003c

--- tests/helper.py
+++ tests/helper.py
@@ -3,4 +3,10 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 import jenkins  # noqa
-from StringIO import StringIO  # noqa
+
+try:
+    # Python 2
+    from StringIO import StringIO  # noqa
+except ImportError:  # pragma: nocover
+    # Python 3
+    from io import StringIO  # noqa
