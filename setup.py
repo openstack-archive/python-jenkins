ba8da27d37583ef9c5f923dc8e4849d251774025
Khai Do <zaro0508@gmail.com>
2014-07-09 15:19:24 -0700

    update version for release
    Change-Id: Ia7d37fd945bd77f44250b15a2136e9866d0a6212

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='python-jenkins',
-      version='0.3.1',
+      version='0.3.2',
       description='Python bindings for the remote Jenkins API',
       author='Ken Conley',
       author_email='kwc@willowgarage.com',
