e0a9686a73040a7768f8f9eee49adae4917bab34
Khai Do <zaro0508@gmail.com>
2014-07-10 10:47:43 -0700

    update version for minor release
    Change-Id: Id8e8c1002ed54f8cb143352964942164896d22fa

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='python-jenkins',
-      version='0.3.2',
+      version='0.3.3',
       description='Python bindings for the remote Jenkins API',
       author='Ken Conley',
       author_email='kwc@willowgarage.com',
