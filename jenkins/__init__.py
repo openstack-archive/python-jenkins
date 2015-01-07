80fdfa5809c318df1377d56e82ac3f241432ef4e
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2015-01-07 15:43:20 -0600

    Eliminate redundant calls to assert_job_exists()
    Now that the jenkins_open() method will raise a specific exception
    if Jenkins returns a 404, some calls to assert_job_exists() now
    become redundant.  Since these calls represent an extra round-trip
    with the Jenkins server, dropping them will result in a reduction
    in time taken for such things as a jenkins-job run.  This change
    eliminates the redundant calls and adjusts the tests as appropriate.
    
    Change-Id: If78dea9ad60d446854d24919bdd7f3f1fee3d733

--- jenkins/__init__.py
+++ jenkins/__init__.py
@@ -470,7 +470,6 @@ class Jenkins(object):
         :param from_name: Name of Jenkins job to copy from, ``str``
         :param to_name: Name of Jenkins job to copy to, ``str``
         '''
-        self.assert_job_exists(from_name)
         self.jenkins_open(Request(
             self.server + COPY_JOB % self._get_encoded_params(locals()), ''))
         self.assert_job_exists(to_name, 'create[%s] failed')
@@ -481,7 +480,6 @@ class Jenkins(object):
         :param from_name: Name of Jenkins job to rename, ``str``
         :param to_name: New Jenkins job name, ``str``
         '''
-        self.assert_job_exists(from_name)
         self.jenkins_open(Request(
             self.server + RENAME_JOB % self._get_encoded_params(locals()), ''))
         self.assert_job_exists(to_name, 'rename[%s] failed')
@@ -491,7 +489,6 @@ class Jenkins(object):
 
         :param name: Name of Jenkins job, ``str``
         '''
-        self.assert_job_exists(name)
         self.jenkins_open(Request(
             self.server + DELETE_JOB % self._get_encoded_params(locals()), ''))
         if self.job_exists(name):
@@ -502,7 +499,6 @@ class Jenkins(object):
 
         :param name: Name of Jenkins job, ``str``
         '''
-        self.assert_job_exists(name)
         self.jenkins_open(Request(
             self.server + ENABLE_JOB % self._get_encoded_params(locals()), ''))
 
@@ -513,7 +509,6 @@ class Jenkins(object):
 
         :param name: Name of Jenkins job, ``str``
         '''
-        self.assert_job_exists(name)
         self.jenkins_open(Request(
             self.server + DISABLE_JOB % self._get_encoded_params(locals()), ''))
 
@@ -569,7 +564,6 @@ class Jenkins(object):
         :param name: Name of Jenkins job, ``str``
         :param config_xml: New XML configuration, ``str``
         '''
-        self.assert_job_exists(name)
         headers = {'Content-Type': 'text/xml'}
         reconfig_url = self.server + CONFIG_JOB % self._get_encoded_params(locals())
         self.jenkins_open(Request(reconfig_url, config_xml, headers))
@@ -602,7 +596,6 @@ class Jenkins(object):
         :param parameters: parameters for job, or ``None``, ``dict``
         :param token: Jenkins API token
         '''
-        self.assert_job_exists(name, 'no such job[%s]')
         return self.jenkins_open(Request(
             self.build_job_url(name, parameters, token), ""))
 
