5b29a91dedd43f71c1711503dc718bd7482e831f
Richard Pijnenburg <richard.pijnenburg@gmail.com>
2015-05-14 11:32:12 +0200

    Add decode to utf8 for python 3.x
    Before this patch python 3.x would cause TypeError's when communicating
    to the jenkins server.
    
    - Added Python 3 support for get_version
    
    Change-Id: I24f0cd46663e5e8b3a06f52e1e0c57dd99a22c5f

--- jenkins/__init__.py
+++ jenkins/__init__.py
@@ -198,7 +198,7 @@ class Jenkins(object):
             except (NotFoundException, EmptyResponseException):
                 self.crumb = False
             else:
-                self.crumb = json.loads(response.decode('utf-8'))
+                self.crumb = json.loads(response)
         if self.crumb:
             req.add_header(self.crumb['crumbRequestField'], self.crumb['crumb'])
 
@@ -282,7 +282,7 @@ class Jenkins(object):
                 raise EmptyResponseException(
                     "Error communicating with server[%s]: "
                     "empty response" % self.server)
-            return response
+            return response.decode('utf-8')
         except HTTPError as e:
             # Jenkins's funky authentication means its nigh impossible to
             # distinguish errors.
@@ -415,7 +415,13 @@ class Jenkins(object):
                 raise EmptyResponseException(
                     "Error communicating with server[%s]: "
                     "empty response" % self.server)
-            return response.info().getheader('X-Jenkins')
+
+            if six.PY2:
+                return response.info().getheader('X-Jenkins')
+
+            if six.PY3:
+                return response.getheader('X-Jenkins')
+
         except (HTTPError, BadStatusLine):
             raise BadHTTPException("Error communicating with server[%s]"
                                    % self.server)
