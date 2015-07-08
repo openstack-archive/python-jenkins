0a1b414f3acfe888eb7bb3d47de2259703cfaa81
Darragh Bailey <dbailey@hp.com>
2015-07-08 13:10:49 +0100

    Add socket timeout tests and helpers
    Add tests that use a simple socket server instance that opens the port
    and ignores all data in order to support testing when the socket timeout
    is set and catching the exception triggered. Also test that when not set
    that the request will continue to remain connected for a period of time
    longer than the other test using a wrapper time limit process.
    
    Includes some helper classes to black hole all requests by simply
    listening using a socketserver but never accepting connections, and a
    time limit function to allow wrapping actions with a time limit when
    checking for blocking behaviour where the call should not return and
    will need to be focibly terminated.
    
    Change-Id: Idab98b0121adb26aed6320ddcf2afbdf2c4428e0

---
+++ tests/helper.py
@@ -0,0 +1,33 @@
+from multiprocessing import Process
+
+from six.moves import socketserver
+
+
+class TestsTimeoutException(Exception):
+    pass
+
+
+def time_limit(seconds, func, *args, **kwargs):
+    # although creating a separate process is expensive it's the only way to
+    # ensure cross platform that we can cleanly terminate after timeout
+    p = Process(target=func, args=args, kwargs=kwargs)
+    p.start()
+    p.join(seconds)
+    p.terminate()
+    if p.exitcode is None:
+        raise TestsTimeoutException
+
+
+class NullServer(socketserver.TCPServer):
+
+    request_queue_size = 1
+
+    def __init__(self, server_address, *args, **kwargs):
+        # TCPServer is old style in python 2.x so cannot use
+        # super() correctly, explicitly call __init__.
+
+        # simply init'ing is sufficient to open the port, which
+        # with the server not started creates a black hole server
+        socketserver.TCPServer.__init__(
+            self, server_address, socketserver.BaseRequestHandler,
+            *args, **kwargs)
