0a609e9a3d8dbab70ddca7439ab462e2699b71f4
Khai Do <zaro0508@gmail.com>
2015-08-07 09:44:56 -0700

    Support ability to install plugins
    Add the ability to install the latest version of a Jenkins plugin.
    Thanks to Fatih Degirmenci for providing the groovy script[1]
    
    [1] http://pastebin.com/9qWeVYSP
    
    Change-Id: I67a0b02710a9f5007a6529d87222e5777e744337

--- tests/test_script.py
+++ tests/test_script.py
@@ -14,3 +14,63 @@ class JenkinsScriptTest(JenkinsTestBase):
             jenkins_mock.call_args[0][0].get_full_url(),
             u'http://example.com/scriptText')
         self._check_requests(jenkins_mock.call_args_list)
+
+    @patch.object(jenkins.Jenkins, 'jenkins_open')
+    def test_install_plugin(self, jenkins_mock):
+        '''Installation of plugins is done with the run_script method
+        '''
+        j = jenkins.Jenkins('http://example.com/', 'test', 'test')
+        j.install_plugin("jabber")
+        self.assertEqual(
+            jenkins_mock.call_args[0][0].get_full_url(),
+            u'http://example.com/scriptText')
+        self._check_requests(jenkins_mock.call_args_list)
+
+    @patch.object(jenkins.Jenkins, 'jenkins_open')
+    @patch.object(jenkins.Jenkins, 'run_script')
+    def test_install_plugin_with_dependencies(self, run_script_mock, jenkins_mock):
+        '''Verify install plugins with dependencies
+        '''
+        j = jenkins.Jenkins('http://example.com/', 'test', 'test')
+        j.install_plugin("jabber")
+        self.assertEqual(len(run_script_mock.call_args_list), 2)
+        self.assertEqual(run_script_mock.call_args_list[0][0][0],
+                         ('Jenkins.instance.updateCenter.getPlugin(\"jabber\")'
+                          '.getNeededDependencies().each{it.deploy()};Jenkins'
+                          '.instance.updateCenter.getPlugin(\"jabber\").deploy();'))
+        self.assertEqual(run_script_mock.call_args_list[1][0][0],
+                         ('Jenkins.instance.updateCenter'
+                          '.isRestartRequiredForCompletion()'))
+
+    @patch.object(jenkins.Jenkins, 'jenkins_open')
+    @patch.object(jenkins.Jenkins, 'run_script')
+    def test_install_plugin_without_dependencies(self, run_script_mock, jenkins_mock):
+        '''Verify install plugins without dependencies
+        '''
+        j = jenkins.Jenkins('http://example.com/', 'test', 'test')
+        j.install_plugin("jabber", include_dependencies=False)
+        self.assertEqual(len(run_script_mock.call_args_list), 2)
+        self.assertEqual(run_script_mock.call_args_list[0][0][0],
+                         ('Jenkins.instance.updateCenter'
+                          '.getPlugin(\"jabber\").deploy();'))
+        self.assertEqual(run_script_mock.call_args_list[1][0][0],
+                         ('Jenkins.instance.updateCenter'
+                          '.isRestartRequiredForCompletion()'))
+
+    @patch.object(jenkins.Jenkins, 'jenkins_open')
+    @patch.object(jenkins.Jenkins, 'run_script')
+    def test_install_plugin_no_restart(self, run_script_mock, jenkins_mock):
+        '''Verify install plugin does not need a restart
+        '''
+        run_script_mock.return_value = u'Result: false\n'
+        j = jenkins.Jenkins('http://example.com/', 'test', 'test')
+        self.assertFalse(j.install_plugin("jabber"))
+
+    @patch.object(jenkins.Jenkins, 'jenkins_open')
+    @patch.object(jenkins.Jenkins, 'run_script')
+    def test_install_plugin_restart(self, run_script_mock, jenkins_mock):
+        '''Verify install plugin needs a restart
+        '''
+        run_script_mock.return_value = u'Result: true\n'
+        j = jenkins.Jenkins('http://example.com/', 'test', 'test')
+        self.assertTrue(j.install_plugin("jabber"))
