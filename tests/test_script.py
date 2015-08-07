03ec1a0fde4c3c3224237df659803fb7e8c2b819
Khai Do <zaro0508@gmail.com>
2015-08-07 09:44:56 -0700

    Support ability to install plugins
    Add the ability to install the latest version of a Jenkins plugin.
    Thanks to Fatih Degirmenci for providing the groovy script[1]
    
    [1] http://pastebin.com/9qWeVYSP
    
    Change-Id: I67a0b02710a9f5007a6529d87222e5777e744337

--- tests/test_script.py
+++ tests/test_script.py
@@ -14,3 +14,15 @@ class JenkinsScriptTest(JenkinsTestBase):
             jenkins_mock.call_args[0][0].get_full_url(),
             u'http://example.com/scriptText')
         self._check_requests(jenkins_mock.call_args_list)
+
+    # installation of plugins is done with the run_script method
+    @patch.object(jenkins.Jenkins, 'jenkins_open')
+    def test_install_plugin(self, jenkins_mock):
+        j = jenkins.Jenkins('http://example.com/', 'test', 'test')
+
+        j.install_plugin("jabber")
+
+        self.assertEqual(
+            jenkins_mock.call_args[0][0].get_full_url(),
+            u'http://example.com/scriptText')
+        self._check_requests(jenkins_mock.call_args_list)
