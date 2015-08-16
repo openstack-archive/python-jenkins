7193bc4918f6ffe5ba794fb0eddc24b53ce49914
Darragh Bailey <daragh.bailey@gmail.com>
2015-08-16 18:08:43 +0100

    Separate tests in separate files and classes
    Split existing tests into separate files and classes to make it easier
    to select a subset to be executed when making modifications.
    
    Add some simple consolidations of test data as example improvements that
    can be extended by consolidating complete tests or sets of tests.
    
    Change-Id: If2380e6f4e848ba68f05868e2ef4186d7912952b

---
+++ tests/test_script.py
@@ -0,0 +1,16 @@
+from mock import patch
+
+import jenkins
+from tests.base import JenkinsTestBase
+
+
+class JenkinsScriptTest(JenkinsTestBase):
+
+    @patch.object(jenkins.Jenkins, 'jenkins_open')
+    def test_run_script(self, jenkins_mock):
+        self.j.run_script(u'println(\"Hello World!\")')
+
+        self.assertEqual(
+            jenkins_mock.call_args[0][0].get_full_url(),
+            u'http://example.com/scriptText')
+        self._check_requests(jenkins_mock.call_args_list)
