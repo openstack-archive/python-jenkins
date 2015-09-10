0ad706629976d6489e26b562400e6faab26356c5
Clark Boylan <clark.boylan@gmail.com>
2015-09-10 15:04:51 -0700

    Add support for quiet down
    This adds a Jenkins.quiet_down() method which can be used to quiet down
    the server. Unit tests included.
    
    Change-Id: I4e229aae6ad766bc6194a643fcd514e5538d6fa7

---
+++ tests/test_quiet_down.py
@@ -0,0 +1,64 @@
+import json
+from mock import patch
+import urllib2
+
+import jenkins
+from tests.base import JenkinsTestBase
+
+
+class JenkinsQuietDownTest(JenkinsTestBase):
+    @patch.object(jenkins.Jenkins, 'jenkins_open')
+    def test_success(self, jenkins_mock):
+        job_info_to_return = {
+            "quietingDown" : True,
+        }
+        jenkins_mock.return_value = json.dumps(job_info_to_return)
+
+        self.j.quiet_down()
+
+        print jenkins_mock.call_args
+        self.assertEqual(
+            jenkins_mock.call_args_list[0][0][0].get_full_url(),
+            u'http://example.com/quietDown')
+        self.assertEqual(
+            jenkins_mock.call_args_list[1][0][0].get_full_url(),
+            u'http://example.com/api/json')
+        self._check_requests(jenkins_mock.call_args_list)
+
+    @patch.object(jenkins.Jenkins, 'jenkins_open')
+    def test_quiet_down_fail(self, jenkins_mock):
+        job_info_to_return = {
+            "quietingDown" : False,
+        }
+        jenkins_mock.return_value = json.dumps(job_info_to_return)
+
+        with self.assertRaises(jenkins.JenkinsException) as context_manager:
+            self.j.quiet_down()
+
+        self.assertEqual(
+            jenkins_mock.call_args_list[0][0][0].get_full_url(),
+            u'http://example.com/quietDown')
+        self.assertEqual(
+            jenkins_mock.call_args_list[1][0][0].get_full_url(),
+            u'http://example.com/api/json')
+        self.assertEqual(
+            str(context_manager.exception),
+            'quiet down failed')
+        self._check_requests(jenkins_mock.call_args_list)
+
+    @patch.object(jenkins.Jenkins, 'jenkins_open')
+    def test_http_fail(self, jenkins_mock):
+        jenkins_mock.side_effect = jenkins.HTTPError(
+            'http://example.com/quietDown',
+            code=401,
+            msg="basic auth failed",
+            hdrs=[],
+            fp=None)
+
+        with self.assertRaises(urllib2.HTTPError):
+            self.j.quiet_down()
+
+        self.assertEqual(
+            jenkins_mock.call_args[0][0].get_full_url(),
+            u'http://example.com/quietDown')
+        self._check_requests(jenkins_mock.call_args_list)
