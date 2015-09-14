c704ab4c9f7fdfe5b766765d3811a5191e7634ab
Clark Boylan <clark.boylan@gmail.com>
2015-09-14 13:15:36 -0700

    Add function to get running builds
    It can be useful to get a list of running builds from jenkins. This is
    particularly useful when you need to take an action on a specific built
    or if you need to know when the jenkins master has finished running all
    builds (say after quieting down). Add a function to return this list.
    
    Change-Id: I5c7dac8076250f94cd2b358c5b153d9410f53aee

--- jenkins/__init__.py
+++ jenkins/__init__.py
@@ -726,6 +726,37 @@ class Jenkins(object):
         self.jenkins_open(Request(
             self._build_url(STOP_BUILD, locals()), b''))
 
+    def get_running_builds(self):
+        '''Return list of running builds.
+
+        Each build is a dict with keys 'name', 'number', and 'url'.
+
+        :returns: List of builds, ``[ { str: str, str: int, str:str} ]``
+        '''
+        builds = []
+        nodes = self.get_nodes()
+        for node in nodes:
+            # the name returned is not the name to lookup when
+            # dealing with master :/
+            if node['name'] == 'master':
+                name = '(master)'
+            else:
+                name = node['name']
+            info = self.get_node_info(name, depth=2)
+            for executor in info['executors']:
+                executable = executor['currentExecutable']
+                if executable:
+                    number = executable['number']
+                    str_num = str(number)
+                    full_name = executable['fullDisplayName']
+                    if full_name.endswith('#' + str_num):
+                        name = full_name[:-(len(str_num) + 2)]
+                    else:
+                        name = full_name
+                    url = executable['url']
+                    builds.append({'name': name, 'number': number, 'url': url})
+        return builds
+
     def get_nodes(self):
         '''Get a list of nodes connected to the Master
 
