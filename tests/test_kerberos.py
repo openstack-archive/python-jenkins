8c2ddff9bfc2b24ce3d55cbccc0ea69d47c98d0c
Lukas Vacek <lucas.vacek@gmail.com>
2015-10-22 16:28:13 +0200

    Add optional kerberos support
    Check if "kerberos" package can be imported at runtime
    (in __init__.py).  If not, everything works as before.
    If "kerberos" package can be imported register a new
    urllib handler (defined in urllib_kerb.py) which adds
    support for kerberos auth.
    
    This urllib handler (urllib_kerb.py) is only triggered
    on 401 (Unauthorized) response, and we try to auth
    with kerberos. If unsuccessful, next 401 handler (if any)
    is triggered.
    
    Change-Id: I1a47e455aa14535a124df950994718a11d7e4f57

---
+++ tests/test_kerberos.py
@@ -0,0 +1,43 @@
+import sys
+
+import kerberos
+assert kerberos  # pyflakes
+from mock import patch, Mock
+
+from jenkins import urllib_kerb
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+
+class KerberosTest(unittest.TestCase):
+
+    @patch('kerberos.authGSSClientResponse')
+    @patch('kerberos.authGSSClientStep')
+    @patch('kerberos.authGSSClientInit')
+    @patch('kerberos.authGSSClientClean')
+    def test_simple(self, clean_mock, init_mock, step_mock, response_mock):
+        headers_from_server = {'www-authenticate': 'Negotiate xxx'}
+
+        init_mock.side_effect = lambda x: (x, "context")
+        response_mock.return_value = "foo"
+
+        parent_mock = Mock()
+        parent_return_mock = Mock()
+        parent_return_mock.headers = {'www-authenticate': "bar"}
+        parent_mock.open.return_value = parent_return_mock
+
+        request_mock = Mock()
+        h = urllib_kerb.HTTPNegotiateHandler()
+        h.add_parent(parent_mock)
+        rv = h.http_error_401(request_mock, "", "", "", headers_from_server)
+
+        init_mock.assert_called()
+        step_mock.assert_called_with("context", "xxx")
+        response_mock.assert_called_with("context")
+        request_mock.add_unredirected_header.assert_called_with(
+            'Authorization', 'Negotiate %s' % "foo")
+        self.assertEqual(rv, parent_return_mock)
+        clean_mock.assert_called_with("context")
