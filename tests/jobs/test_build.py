b18888743e5c0ee01550e85dee0063b158311785
Ken Dreyer <ktdreyer@ktdreyer.com>
2017-10-13 11:54:32 -0600

    build_job: return queue item identifier
    Prior to this change, build_job() would always return an empty string
    (the empty HTTP body from the POST request).
    
    Read the HTTP response's Location header and return the queue ID number.
    
    Clients can use this number to query server for the the upcoming job's
    status.
    
    Change-Id: I63a5adfc1a82e21b66195b51f33fa838e33b530f

--- tests/jobs/test_build.py
+++ tests/jobs/test_build.py
@@ -1,76 +1,78 @@
-from mock import patch
+from mock import patch, Mock
+import six
 
-import jenkins
 from tests.jobs.base import JenkinsJobsTestBase
 
 
 class JenkinsBuildJobTest(JenkinsJobsTestBase):
 
-    @patch.object(jenkins.Jenkins, 'jenkins_open')
-    def test_simple(self, jenkins_mock):
-        jenkins_mock.side_effect = [
-            {'foo': 'bar'},
-        ]
+    def mock_response(self):
+        """ Return a fake response suitable for returning from urlopen(). """
+        mock_response = Mock()
+        location = self.make_url('/queue/item/25/')
+        if six.PY2:
+            config = {'info.return_value.getheader.return_value': location}
+        if six.PY3:
+            config = {'getheader.return_value': location}
+        mock_response.configure_mock(**config)
+        mock_response.read.return_value = ['{}', '']
+        return mock_response
+
+    @patch('jenkins.urlopen')
+    def test_simple(self, urlopen_mock):
+        urlopen_mock.side_effect = [None, self.mock_response()]
 
         build_info = self.j.build_job(u'Test Job')
 
-        self.assertEqual(jenkins_mock.call_args[0][0].get_full_url(),
+        self.assertEqual(urlopen_mock.call_args[0][0].get_full_url(),
                          self.make_url('job/Test%20Job/build'))
-        self.assertEqual(build_info, {'foo': 'bar'})
-        self._check_requests(jenkins_mock.call_args_list)
+        self.assertEqual(build_info, 25)
+        self._check_requests(urlopen_mock.call_args_list)
 
-    @patch.object(jenkins.Jenkins, 'jenkins_open')
-    def test_in_folder(self, jenkins_mock):
-        jenkins_mock.side_effect = [
-            {'foo': 'bar'},
-        ]
+    @patch('jenkins.urlopen')
+    def test_in_folder(self, urlopen_mock):
+        urlopen_mock.side_effect = [None, self.mock_response()]
 
         build_info = self.j.build_job(u'a Folder/Test Job')
 
-        self.assertEqual(jenkins_mock.call_args[0][0].get_full_url(),
+        self.assertEqual(urlopen_mock.call_args[0][0].get_full_url(),
                          self.make_url('job/a%20Folder/job/Test%20Job/build'))
-        self.assertEqual(build_info, {'foo': 'bar'})
-        self._check_requests(jenkins_mock.call_args_list)
+        self.assertEqual(build_info, 25)
+        self._check_requests(urlopen_mock.call_args_list)
 
-    @patch.object(jenkins.Jenkins, 'jenkins_open')
-    def test_with_token(self, jenkins_mock):
-        jenkins_mock.side_effect = [
-            {'foo': 'bar'},
-        ]
+    @patch('jenkins.urlopen')
+    def test_with_token(self, urlopen_mock):
+        urlopen_mock.side_effect = [None, self.mock_response()]
 
         build_info = self.j.build_job(u'TestJob', token='some_token')
 
-        self.assertEqual(jenkins_mock.call_args[0][0].get_full_url(),
+        self.assertEqual(urlopen_mock.call_args[0][0].get_full_url(),
                          self.make_url('job/TestJob/build?token=some_token'))
-        self.assertEqual(build_info, {'foo': 'bar'})
-        self._check_requests(jenkins_mock.call_args_list)
+        self.assertEqual(build_info, 25)
+        self._check_requests(urlopen_mock.call_args_list)
 
-    @patch.object(jenkins.Jenkins, 'jenkins_open')
-    def test_in_folder_with_token(self, jenkins_mock):
-        jenkins_mock.side_effect = [
-            {'foo': 'bar'},
-        ]
+    @patch('jenkins.urlopen')
+    def test_in_folder_with_token(self, urlopen_mock):
+        urlopen_mock.side_effect = [None, self.mock_response()]
 
         build_info = self.j.build_job(u'a Folder/TestJob', token='some_token')
 
-        self.assertEqual(jenkins_mock.call_args[0][0].get_full_url(),
+        self.assertEqual(urlopen_mock.call_args[0][0].get_full_url(),
                          self.make_url('job/a%20Folder/job/TestJob/build?token=some_token'))
-        self.assertEqual(build_info, {'foo': 'bar'})
-        self._check_requests(jenkins_mock.call_args_list)
+        self.assertEqual(build_info, 25)
+        self._check_requests(urlopen_mock.call_args_list)
 
-    @patch.object(jenkins.Jenkins, 'jenkins_open')
-    def test_with_parameters_and_token(self, jenkins_mock):
-        jenkins_mock.side_effect = [
-            {'foo': 'bar'},
-        ]
+    @patch('jenkins.urlopen')
+    def test_with_parameters_and_token(self, urlopen_mock):
+        urlopen_mock.side_effect = [None, self.mock_response()]
 
         build_info = self.j.build_job(
             u'TestJob',
             parameters={'when': 'now', 'why': 'because I felt like it'},
             token='some_token')
 
-        self.assertTrue('token=some_token' in jenkins_mock.call_args[0][0].get_full_url())
-        self.assertTrue('when=now' in jenkins_mock.call_args[0][0].get_full_url())
-        self.assertTrue('why=because+I+felt+like+it' in jenkins_mock.call_args[0][0].get_full_url())
-        self.assertEqual(build_info, {'foo': 'bar'})
-        self._check_requests(jenkins_mock.call_args_list)
+        self.assertTrue('token=some_token' in urlopen_mock.call_args[0][0].get_full_url())
+        self.assertTrue('when=now' in urlopen_mock.call_args[0][0].get_full_url())
+        self.assertTrue('why=because+I+felt+like+it' in urlopen_mock.call_args[0][0].get_full_url())
+        self.assertEqual(build_info, 25)
+        self._check_requests(urlopen_mock.call_args_list)
