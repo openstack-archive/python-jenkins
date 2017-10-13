641894cc25aa50a2696fc1bc94ab39ae6fecc33b
Ken Dreyer <ktdreyer@ktdreyer.com>
2017-10-13 11:54:38 -0600

    add get_queue_item() method
    Pass in a queue ID number to discover a job's status, and possibly a job
    URL.
    
    Change-Id: Ic5584b6e19056c94c5c3676cbd81993d264b47f6

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -70,3 +70,42 @@ class JenkinsQueueInfoTest(JenkinsTestBase):
             jenkins_mock.call_args[0][0].get_full_url(),
             self.make_url('queue/api/json?depth=0'))
         self._check_requests(jenkins_mock.call_args_list)
+
+
+class JenkinsQueueItemTest(JenkinsTestBase):
+    @patch.object(jenkins.Jenkins, 'jenkins_open')
+    def test_simple(self, jenkins_mock):
+        queue_item_to_return = {
+            u'_class': u'hudson.model.Queue$LeftItem',
+            u'actions': [{u'_class': u'hudson.model.CauseAction',
+                          u'causes': [{u'_class': u'hudson.model.Cause$UserIdCause',
+                                       u'shortDescription': u'Started by user Bob',
+                                       u'userId': u'bsmith',
+                                       u'userName': u'Bob'}]}],
+            u'blocked': False,
+            u'buildable': False,
+            u'cancelled': False,
+            u'executable': {u'_class': u'hudson.model.FreeStyleBuild',
+                            u'number': 198,
+                            u'url': u'http://your_url/job/my_job/198/'},
+            u'id': 25,
+            u'inQueueSince': 1507914654469,
+            u'params': u'',
+            u'stuck': False,
+            u'task': {u'_class': u'hudson.model.FreeStyleProject',
+                      u'color': u'red',
+                      u'name': u'my_job',
+                      u'url': u'http://your_url/job/my_job/'},
+            u'url': u'queue/item/25/',
+            u'why': None,
+        }
+
+        jenkins_mock.return_value = json.dumps(queue_item_to_return)
+
+        queue_item = self.j.get_queue_item(25)
+
+        self.assertEqual(queue_item, queue_item_to_return)
+        self.assertEqual(
+            jenkins_mock.call_args[0][0].get_full_url(),
+            self.make_url('queue/item/25/api/json'))
+        self._check_requests(jenkins_mock.call_args_list)
