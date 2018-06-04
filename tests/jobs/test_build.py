985cc7b2da4fd4dcc8b715ee37774cf39cd7183b
Dennis Dmitriev <ddmitriev@mirantis.com>
2018-06-04 21:02:20 +0300

    Check for 'Location' header in the response
    If the keep_alive is not available because of using reverse
    proxy, the header 'Content-Lenght' may not be available,
    causing the exception:
    
     Error communicating with server[...]: empty response
    
    However, if 'Location' header is present, the response is valid.
    
    Add unit tests:
    - build_job must pass even if no 'content-lenght'
      in response header
    - build_job must fail if no 'location' in response header
    
    Change-Id: I4da6dd19f9d8302a76652a3686a9377f9a2503a6
    Closes-Bug:#1775047

--- tests/jobs/test_build.py
+++ tests/jobs/test_build.py
@@ -1,5 +1,6 @@
 from mock import patch
 
+import jenkins
 from tests.helper import build_response_mock
 from tests.jobs.base import JenkinsJobsTestBase
 
@@ -16,6 +17,49 @@ class JenkinsBuildJobTest(JenkinsJobsTestBase):
         self.assertEqual(session_send_mock.call_args[0][1].url,
                          self.make_url('job/Test%20Job/build'))
         self.assertEqual(queue_id, 25)
+
+    @patch('jenkins.requests.Session.send', autospec=True)
+    def test_assert_no_location(self, session_send_mock):
+        session_send_mock.return_value = build_response_mock(302, {})
+
+        with self.assertRaises(jenkins.EmptyResponseException) as context_mgr:
+            self.j.build_job(u'Test Job')
+
+        self.assertEqual(
+            str(context_mgr.exception),
+            "Header 'Location' not found in response from server[{0}]".format(
+                self.make_url('')))
+
+    @patch.object(jenkins.Jenkins, 'maybe_add_crumb')
+    @patch('jenkins.requests.Session.send', autospec=True)
+    def test_simple_no_content_lenght(self, session_send_mock,
+                                      maybe_add_crumb_mock):
+        maybe_add_crumb_mock.return_value = None
+        session_send_mock.return_value = build_response_mock(
+            201, None, add_content_length=False,
+            headers={'Location': self.make_url('/queue/item/25/')})
+
+        queue_id = self.j.build_job(u'Test Job')
+
+        self.assertEqual(session_send_mock.call_args[0][1].url,
+                         self.make_url('job/Test%20Job/build'))
+        self.assertEqual(queue_id, 25)
+
+    @patch.object(jenkins.Jenkins, 'maybe_add_crumb')
+    @patch('jenkins.requests.Session.send', autospec=True)
+    def test_assert_no_content_lenght_no_location(self, session_send_mock,
+                                                  maybe_add_crumb_mock):
+        maybe_add_crumb_mock.return_value = None
+        session_send_mock.return_value = build_response_mock(
+            201, None, add_content_length=False)
+
+        with self.assertRaises(jenkins.EmptyResponseException) as context_mgr:
+            self.j.build_job(u'Test Job')
+
+        self.assertEqual(
+            str(context_mgr.exception),
+            'Error communicating with server[{0}]: empty response'.format(
+                self.make_url('')))
 
     @patch('jenkins.requests.Session.send', autospec=True)
     def test_in_folder(self, session_send_mock):
