4e444793c3f76ddbc71f103fcf560832020f4c74
Tomas Janousek <tomas.janousek@gooddata.com>
2018-08-13 14:17:29 +0200

    Merge jobs_in_folder_named_job into jobs_in_folder
    Now that jobs_in_folder isn't reused in all other get_(all_)jobs tests
    there's no more any reason to keep jobs_in_folder_named_job a separate
    test.
    
    Change-Id: Ic51a4b2cac3105cf9406d2a7864943a7df1248be

--- tests/jobs/test_getall.py
+++ tests/jobs/test_getall.py
@@ -14,8 +14,8 @@ class JenkinsGetAllJobsTest(JenkinsGetJobsTestBase):
         jobs_info = self.j.get_all_jobs()
 
         expected_fullnames = [
-            u"my_job1", u"my_folder1", u"my_job2",
-            u"my_folder1/my_job3", u"my_folder1/my_job4"
+            u"my_job1", u"my_folder1", u"my_job2", u"job",
+            u"my_folder1/my_job3", u"my_folder1/my_job4", u"job/my_job"
         ]
         self.assertEqual(len(expected_fullnames), len(jobs_info))
         got_fullnames = [job[u"fullname"] for job in jobs_info]
@@ -23,7 +23,8 @@ class JenkinsGetAllJobsTest(JenkinsGetJobsTestBase):
 
         expected_request_urls = [
             self.make_url('api/json'),
-            self.make_url('job/my_folder1/api/json')
+            self.make_url('job/my_folder1/api/json'),
+            self.make_url('job/job/api/json')
         ]
         self.assertEqual(expected_request_urls,
                          self.got_request_urls(jenkins_mock))
@@ -98,25 +99,6 @@ class JenkinsGetAllJobsTest(JenkinsGetJobsTestBase):
             self.make_url('api/json'),
             self.make_url('job/my_folder1/api/json'),
             self.make_url('job/my_folder1/job/my%20spaced%20folder/api/json')
-        ]
-        self.assertEqual(expected_request_urls,
-                         self.got_request_urls(jenkins_mock))
-
-    @patch.object(jenkins.Jenkins, 'jenkins_open')
-    def test_folder_named_job(self, jenkins_mock):
-        jenkins_mock.side_effect = map(
-            json.dumps, self.jobs_in_folder_named_job)
-
-        jobs_info = self.j.get_all_jobs()
-
-        expected_fullnames = [u"job", u"job/my_job"]
-        self.assertEqual(len(expected_fullnames), len(jobs_info))
-        got_fullnames = [job[u"fullname"] for job in jobs_info]
-        self.assertEqual(expected_fullnames, got_fullnames)
-
-        expected_request_urls = [
-            self.make_url('api/json'),
-            self.make_url('job/job/api/json'),
         ]
         self.assertEqual(expected_request_urls,
                          self.got_request_urls(jenkins_mock))
