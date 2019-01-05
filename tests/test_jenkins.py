ada747e2b18e72fff65a777a06295a634065558e
Jan Zerebecki <jzerebecki@suse.com>
2019-01-05 02:08:49 +0100

    When updating jobs the response body may be empty
    Other specific requests may need a body. Thus remove the general check
    for an empty body and add a more specific one where needed.
    
    I noticed this because when updating a Jenkins job with Jenkins Job
    Builder I got an EmptyResponseException even though updating worked
    correctly.
    
    Change-Id: Ie144e6fb0b922a681e6c65ffbe5120da72a495f0

--- tests/test_jenkins.py
+++ tests/test_jenkins.py
@@ -108,7 +108,7 @@ class JenkinsMaybeAddCrumbTest(JenkinsTestBase):
     @patch.object(jenkins.Jenkins, 'jenkins_open')
     def test_return_empty_response(self, jenkins_mock):
         "Don't try to create crumb header from an empty response"
-        jenkins_mock.side_effect = jenkins.EmptyResponseException("empty response")
+        jenkins_mock.return_value = ''
         request = jenkins.requests.Request('GET', 'http://example.com/job/TestJob')
 
         self.j.maybe_add_crumb(request)
@@ -254,11 +254,9 @@ class JenkinsOpenTest(JenkinsTestBase):
         j = jenkins.Jenkins('http://example.com', 'test', 'test')
         self.assertTrue(j.wait_for_normal_op(0))
 
-    @patch.object(jenkins.Jenkins, 'jenkins_open',
-                  side_effect=jenkins.EmptyResponseException())
     @patch.object(jenkins.Jenkins, 'get_version',
-                  side_effect=jenkins.EmptyResponseException())
-    def test_wait_for_normal_op__empty_response(self, version_mock, jenkins_mock):
+                  side_effect=jenkins.BadHTTPException())
+    def test_wait_for_normal_op__failed_response(self, version_mock):
         j = jenkins.Jenkins('http://example.com', 'test', 'test')
         self.assertFalse(j.wait_for_normal_op(0))
 
